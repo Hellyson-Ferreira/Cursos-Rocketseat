542e6dad3d30277382bb21c3ada60089d178b308
Hellyson Ferreira <37805353+Hellyson-Ferreira@users.noreply.github.com>
2021-03-20 19:51:17 -0300

    aula 7 - estado e imutabilidade

--- GoStack Bootcamp/frontend/src/App.js
+++ GoStack Bootcamp/frontend/src/App.js
@@ -1,25 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from './components/Header.js'
 function App() {
+    const [projects,setProjects] = useState(['Fazer CC', 'Estudar flutter'])
+
+    function handleAddProject(){
+        setProjects([...projects,  `Novo projeto ${Date.now()}`])
+    }
     return <>
-        <Header title="Home " >
-            <ul>
-                <li>menu1</li>
-                <li>menu2</li>
-                <li>menu3</li>
-                <li>menu4</li>
-                <li>menu5</li>
-            </ul>
-        </Header>
-        <Header title="Projects">
+      
+        <Header title="Projects"/>
         <ul>
-                <li>menu1</li>
-                <li>menu2</li>
-                <li>menu3</li>
-                <li>menu4</li>
-                <li>menu5</li>
-            </ul>
-        </Header>
+            {projects.map(project => <li key={project} >{project}</li>)}
+        </ul>
+
+        <button style={{color: '#7159C1'}} onClick={handleAddProject}>Adicionar Projeto</button>
+        
 
     </>
 
